c3c774af5175838c35488f04fe6c43c63372d4be
Kmo73 <nelsoncamilopatino@gmail.com>
2024-02-26 22:21:38 -0300

    ..prevParcial

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Card from './components/Card'
+import styles from '../styles/styles.module.css'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <h1>Carga de estudiantes</h1>
+      <form>
+
+      </form>
+      <Card />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '../styles/styles.module.css'
+
+const Card = () => {
+  return (
+    <div>Hola, soy un tarjeta</div>
+  )
+}
+
+export default Card
